0c54d502513fea436697ae67f3deefc2ad7ed820
Pelumi <pelumi.olalekan.g@gmail.com>
2023-04-20 04:12:28 +0000

    updated task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -9,10 +9,11 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     const result = [];
     values.forEach((element) => {
       if (element.status === 'fulfilled') {
-        //	console.log('status: ', element.status)
+        // console.log('status: ', element.status)
         result.push({ status: element.status, value: element.value });
       } else {
-        result.push({ status: element.status, value: element.reason });
+        // console.log(`${element.reason}`)
+        result.push({ status: element.status, value: `${element.reason}` });
       }
     });
     return result;
