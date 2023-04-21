cef011db541822b8240151af5a1d541c289f3257
pelumi <polalekan@gmail.com>
2023-04-21 23:47:55 +0100

    task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,38 +1,28 @@
-export default class HolbertonCourse {
-    constructor(name, length, students) {
-      if (typeof name !== 'string') throw new TypeError('name must be a string');
-      if (!Number.isInteger(length)) throw new TypeError('length must be a number');
-      if (!Array.isArray(students)) throw new TypeError('students type must be an Array');
-
-      this._name = name;
-      this._length = length;
-      this._students = students;
-    }
-
-    get name() {
-      return this._name;
-    }
-
-    set name(val) {
-      if (typeof val !== 'string') throw new TypeError('name must be a string');
-      this._name = val;
-    }
-
-    get length() {
-      return this._length;
-    }
-
-    set length(val) {
-      if (!Number.isInteger(val)) throw new TypeError('length must be a number');
-      this._length = val;
-    }
-
-    get students() {
-      return this._students;
-    }
-
-    set students(val) {
-      if (!Array.isArray(val)) throw new TypeError('students type must be an Array');
-      this._students = val;
-    }
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(val) {
+    // if (!Number.isInteger(val)) throw new TypeError("length must be a number");
+    this._code = val;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    // if (typeof val !== "string") throw new TypeError("name must be a string");
+    this._name = val;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`
+  }
+}
