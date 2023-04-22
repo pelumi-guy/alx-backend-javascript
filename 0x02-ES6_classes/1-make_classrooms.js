36db2a8f9ced95c51aa8036aa21af291950eb80b
pelumi <polalekan@gmail.com>
2023-04-22 03:46:00 +0100

    task 0 and 1 again

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,9 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-const initializeRooms = () => [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-
-export default initializeRooms;
+export default function initializeRooms() {
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+  const newList = [class1, class2, class3];
+  return (newList);
+}
