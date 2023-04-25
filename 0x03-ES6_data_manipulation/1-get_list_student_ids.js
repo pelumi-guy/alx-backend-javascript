93fbf7e76c539a3a7a96a4449d8d06e5ae2597d6
pelumi <polalekan@gmail.com>
2023-04-25 07:00:45 +0100

    task 0 - 2

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+const getListStudentIds = (arr) => {
+
+    if (!Array.isArray(arr)) return [];
+
+    const newArr = arr.map((obj) => obj.id);
+
+    return newArr;
+}
+
+export default getListStudentIds;
---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,8 @@
+const getStudentsByLocation = (students, city) => {
+
+    const result = students.filter((student) => student.location === city);
+
+    return result;
+}
+
+export default getStudentsByLocation;
