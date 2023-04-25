116d65f9b1004f02cafd9d6226580aea9d21422d
pelumi <polalekan@gmail.com>
2023-04-25 15:23:20 +0100

    task 5

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 const getStudentIdsSum = (students) => students.reduce((acc, student) => acc + student.id, 0);
 
-export default getStudentIdsSum
+export default getStudentIdsSum;
--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,22 +1,23 @@
+/* eslint-disable no-param-reassign */
+
 const updateStudentGradeByCity = (students, city, newGrades) => {
+  const cityStudents = students.filter((student) => student.location === city);
+  // console.log('cityStudents: ', cityStudents);
 
-    const cityStudents = students.filter((student) => student.location === city);
-    // console.log('cityStudents: ', cityStudents);
+  const result = cityStudents.map((student) => {
+    const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
+    // console.log('grade length: ', studentGrade.length);
 
-    const result = cityStudents.map((student) => {
-        const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
-        // console.log('grade length: ', studentGrade.length);
+    if (studentGrade.length === 0) {
+      student.grade = 'N/A';
+    } else {
+      student.grade = studentGrade[0].grade;
+    }
 
-        if (studentGrade.length === 0) {
-            student.grade = 'N/A';
-        } else {
-            student.grade = studentGrade[0].grade;
-        }
+    return student;
+  });
 
-        return student;
-    })
-
-    return result;
-}
+  return result;
+};
 
 export default updateStudentGradeByCity;
