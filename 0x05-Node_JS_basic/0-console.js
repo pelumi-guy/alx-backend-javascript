b9c485d252d349bc228d483a7173b0fdb8a7dec4
pelumi <polalekan@gmail.com>
2023-06-13 04:12:49 +0100

    05 - Task 0 updated

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,7 @@
 // 0. Executing basic javascript with Node JS
 
-module.exports = function displayMessage (arg) {
+function displayMessage(arg) {
   console.log(arg);
 };
+
+module.exports = displayMessage;
